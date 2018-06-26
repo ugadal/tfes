6883c3a8bf88883526f71608ac297726ae3bd744
Deudjui <deudjuimartin@yahoo.fr>
2018-06-26 14:01:08 +0000

    Modif

--- martin/cgi/getfich.py
+++ martin/cgi/getfich.py
@@ -9,34 +9,56 @@ idp=f.getvalue("idp")
 print "Content-type:text/html"
 print
 #~ print idp
+
+
+li1=["id","nom","prenom","sexe","age","ville","quartier","tel","email"]
 c.execute("""SELECT * from patient where id=?""",(idp,))
-IPatient=c.fetchall()
-print "<table border=6 cellspacing=2 cellpadding=1>"
-print "<tr>"
-for i in range(len(IPatient)):
-	p=IPatient[i]
-	for j in range(len(p)):
-		print "<td>",p[j],"</td>"
-	print "</tr>"
+b=c.fetchall()
 
-print "</table>"
-print "<br/><br/>"
+for row in b:
+	print "<caption>Information du patient </caption>"
+	print "<table cellspacing=5 cellpadding=2>"
+	print "<tr>"
+	for i in range(len(row)):
+		print """<th style="background-color:powderblue;">""",li1[i],"</th>","""<td style="background-color:#00FF80;">""", row[i],"</td>","</tr>"
+	
+	print "</table>"	
 
 
-print "<br><hr>"
+
+
+
+
+
+
+
+#~ c.execute("""SELECT * from patient where id=?""",(idp,))
+#~ IPatient=c.fetchall()
+#~ print "<table border=6 cellspacing=2 cellpadding=1>"
+#~ print "<tr>"
+#~ for i in range(len(IPatient)):
+	#~ p=IPatient[i]
+	#~ for j in range(len(p)):
+		#~ print "<td>",p[j],"</td>"
+	#~ print "</tr>"
+
+#~ print "</table>"
+
+
+print "<hr>"
 
 
 
 
 #####################
-TA=["biochimie","bacteriologie","hemato_parasitologie","immuno_serologie"]
+#~ TA=["biochimie","bacteriologie","hemato_parasitologie","immuno_serologie"]
 
-for ta in TA:
-	print "resultats de %s :<br>"%ta
-	cmd="""select * from %s where ID_patient= %s"""%(ta,idp)
-	c.execute(cmd)
-	res=c.fetchall()
-	if res:print res
+#~ for ta in TA:
+	#~ print "resultats de %s :<br>"%ta
+	#~ cmd="""select * from %s where ID_patient= %s"""%(ta,idp)
+	#~ c.execute(cmd)
+	#~ res=c.fetchall()
+	#~ if res:print res
 #####################
 
 print """<button onclick=deletepatient(%s)>delete this patient</button>"""%idp
