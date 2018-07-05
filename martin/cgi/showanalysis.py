d12240d1341b63aaf5d83e840561d3aa9a6c07f3
Deudjui <deudjuimartin@yahoo.fr>
2018-07-05 14:35:49 +0000

    Modification de l'affichage des resultats

--- martin/cgi/showanalysis.py
+++ martin/cgi/showanalysis.py
@@ -19,12 +19,37 @@ c.execute(cmd)
 cn=zip(*c.description)[0] #nom des colonnes
 r=c.fetchone()
 
-print "<table border=1 cellspacing=5 cellpadding=1>"
-print "<tr>"
+#~ print "<table border=1 cellspacing=5 cellpadding=1>"
+#~ print "<tr>"
+#~ for k,v in zip(cn[2:],r[2:]):
+    #~ if mpa.has_key(k):k=mpa[k]
+    #~ print "<th style='background-color:powderblue;'>",k,"</th>","<td style='background-color:#F8E6E0;'>",v,"</td>","</tr>"
+    
+#~ print "</table><br/>"
+#~ print """<button onclick=deleteresult("%s","%s","%s")>supprimer ce resultat</button>"""%(idp,table,ida)
+
+
+lign1=[]
+lign2=[]
+ligntot=[lign1,lign2]
 for k,v in zip(cn[2:],r[2:]):
     if mpa.has_key(k):k=mpa[k]
-    print "<th style='background-color:powderblue;'>",k,"</th>","<td style='background-color:#F8E6E0;'>",v,"</td>","</tr>"
-    
-print "</table><br/>"
+    lign1.append(k)
+    lign2.append(v)
+print "<table border=1 cellspacing=2 cellpadding=5>"
+for i in range(len(ligntot)):
+	print "<tr>"
+	for j in range(len(lign1)):
+		if i==0:
+			print "<th style='background-color:powderblue;'>"
+			print ligntot[i][j]
+			print "</th>"
+		else:
+			print "<td style='background-color:#F8E6E0;'>"
+			print ligntot[i][j]
+			print "</td>"
+			
+	print "</tr>"
+print "</table><br/>"    
 print """<button onclick=deleteresult("%s","%s","%s")>supprimer ce resultat</button>"""%(idp,table,ida)
 
