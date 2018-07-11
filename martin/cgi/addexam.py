dd153d05417658bcc5dbcfc03ebfc7d7a91ad23e
Deudjui <deudjuimartin@yahoo.fr>
2018-07-11 00:41:23 +0000

    Modif

--- martin/cgi/addexam.py
+++ martin/cgi/addexam.py
@@ -10,32 +10,62 @@ print "Content-type:text/html"
 print
 
 
-print "<H2><i>Fiche de paillase de  </i>: <b>%s</b></H2><hr/>"%exam
-print "<h3>Introduisez vos resultats d'analyse(s) ici:</h3>"
+print """<H2><font color="#610B0B"><i>Fiche de paillase de : </i></font> <b>%s</b></H2><hr/>"""%exam
+print """<h3><font color="#B40404">Introduisez vos resultats d'analyse(s) ici:</font></h3>"""
 print """<form id="examform" onsubmit="return examform(%s);">  """%ID_patient
-ete=[]
-c.execute("pragma table_info(%s);"%exam)
-i=0
-j=0
-for c in c.fetchall():
-	ete.append(c[1])
-	if i<2:
-		print 
-	else:
-		if i==2:
-			print """<b>Date:</b> <input type="date" name="%s" required value="2018-07-22" placeholder="%s"> <br/>"""%(c[1],c[1])
-		else:
-			print """ <input type="text" name="%s" value="" placeholder="%s">"""%(c[1],c[1])
-	if j>3:
-		print "<br/>"
-		j=0
-	i+=1
+print """<b>Date:</b> <input type="date" name="date" required value="" placeholder="2018-07-22"> <br/>"""
 
-del ete[0]
+
+c.execute('''select * from table_analyse where nom1="%s";'''%exam)
+z=0
+for i in c:
+	if i[3]=="radio":
+		z+=1
+		if z==1:
+			print """<hr/><p><i><b>%s</b></i></p> """%i[6]
+			print """ <input type="radio" name="%s" id="%s" value="%s">"""%(i[2],i[2],i[5])
+			print """<label for="%s"> %s</label><br/>"""%(i[2],i[5])
+			z+=1
+		else:
+			print """ <input type="radio" name="%s" id="%s" value="%s">"""%(i[2],i[2],i[5])
+			print """<label for="%s"> %s</label><br/>"""%(i[2],i[5])
+			if z>i[4]:
+				z=0
+			else:
+				z=z
+	else:
+		print """ <input type="text" name="%s" value="" placeholder="%s">"""%(i[2],i[2])
+		z=0
+
 print """<br><br><button>Enregistrer</button>   
 <input type=hidden name="ID_patient" value=%s>     
 <input type=hidden name="exam" value=%s>     
 </form> """%(ID_patient,exam)
 print "<br/>"
 
+###################################################################################################
 
+#~ ete=[]
+#~ c.execute("pragma table_info(%s);"%exam)
+#~ i=0
+#~ j=0
+#~ for c in c.fetchall():
+	#~ ete.append(c[1])
+	#~ if i<2:
+		#~ print 
+	#~ else:
+		#~ if i==2:
+			#~ print """<b>Date:</b> <input type="date" name="%s" required value="" placeholder="2018-07-22"> <br/>"""%(c[1])
+		#~ else:
+			#~ print """ <input type="text" name="%s" value="" placeholder="%s">"""%(c[1],c[1])
+	#~ if j>3:
+		#~ print "<br/>"
+		#~ j=0
+	#~ i+=1
+
+#~ del ete[0]
+#~ print """<br><br><button>Enregistrer</button>   
+#~ <input type=hidden name="ID_patient" value=%s>     
+#~ <input type=hidden name="exam" value=%s>     
+#~ </form> """%(ID_patient,exam)
+#~ print "<br/>"
--- martin/cgi/inittables.py
+++ martin/cgi/inittables.py
@@ -11,37 +11,85 @@ C = sqlite3.connect('db/impm.db')
 c = C.cursor()
 
 def CreateTable():
-	c.execute('''CREATE TABLE `bacteriologie` (
+	c.execute('''CREATE TABLE `table_analyse` (
   `id` integer PRIMARY KEY,
-  `ID_patient` int(15),
-  `date` date,
-  `type_examen` varchar(255),
-  `macroscopie` text,
-  `etat_frais` text,
-  `comptage_cellules` int(15),
-  `coloration_gram` text,
-  `coloration_ziehl` text,
-  `milieu_culture` text,
-  `observation` text,
-  `conclusion` text,
-  `technicien` varchar(255));
+  `nom1` varchar(255),
+  `nom2` varchar(255),
+  `input_type` varchar(255),
+  `nb_val` int(15),
+  `val` varchar(255),
+  `inst` varchar(255));
+  
  ''')
 
-#~ def AddEntry(nom,prenom,sexe,age,ville,quartier,tel,email):
-    #~ c.execute('''insert into patient(nom,prenom,sexe,age,ville,quartier,tel,email)
-    #~ VALUES (?,?,?,?,?,?,?,?)''',(nom,prenom,sexe,age,ville,quartier,tel,email))
+def AddEntry(nom1,nom2,input_type,nb_val,val,inst):
+    c.execute('''insert into table_analyse(nom1,nom2,input_type,nb_val,val,inst)
+    VALUES (?,?,?,?,?,?)''',(nom1,nom2,input_type,nb_val,val,inst))
 
 CreateTable()
 
-#~ AddEntry('Deudjui','martin','M',33,'Mouscron','Dottignies',"0465513010","deudjuimartin@yahoo.fr")
-#~ AddEntry('Deker','patrick','M',17,'Mons','Caregnon',"0410564567","exemple@yahoo.com")
-#~ AddEntry('Ben','charlie','M',45,'Namur','Coin',"0410564567","exemple@yahoo.com")
-#~ AddEntry('Van','charlotte','F',23,'Bruxelles','Molemberk', "0410564567","exemple@yahoo.com")
-#~ AddEntry('Van','charlotte','F',23,'Bruxelles','Molemberk', "0410564567","exemple@yahoo.com")
+AddEntry("bacteriologie","macroscopie","text",1,"","")
+AddEntry("bacteriologie","etat_frais","text",1,"","")
+AddEntry("bacteriologie","comptage_cellules","text",1,"","")
+AddEntry("bacteriologie","coloration_gram","radio",2,"positif","Coloration Gram")
+AddEntry("bacteriologie","coloration_gram","radio",2,"negatif","Coloration gram")
+AddEntry("bacteriologie","coloration_ziehl","text",1,"","")
+AddEntry("bacteriologie","milieu_culture","text",1,"","")
+AddEntry("bacteriologie","observation","text",1,"","")
+AddEntry("bacteriologie","conclusion","text",1,"","")
+AddEntry("bacteriologie","technicien","text",1,"","")
+
+AddEntry("biochimie","S_uree","text",1,"","")
+AddEntry("biochimie","S_creat","text",1,"","")
+AddEntry("biochimie","S_gluc","text",1,"","")
+AddEntry("biochimie","S_ac_ur","text",1,"","")
+AddEntry("biochimie","S_chol_t","text",1,"","")
+AddEntry("biochimie","S_sdl","text",1,"","")
+AddEntry("biochimie","S_ldl","text",1,"","")
+AddEntry("biochimie","S_tg","text",1,"","")
+AddEntry("biochimie","E_got","text",1,"","")
+AddEntry("biochimie","E_gpt","text",1,"","")
+AddEntry("biochimie","I_na","text",1,"","")
+AddEntry("biochimie","I_k","text",1,"","")
+AddEntry("biochimie","I_cl","text",1,"","")
+AddEntry("biochimie","I_ca","text",1,"","")
+AddEntry("biochimie","I_mg","text",1,"","")
+AddEntry("biochimie","technicien","text",1,"","")
+
+AddEntry("hemato_parasitologie","gs","radio",4,"A","Groupe sanguin")
+AddEntry("hemato_parasitologie","gs","radio",4,"B","Groupe sanguin")
+AddEntry("hemato_parasitologie","gs","radio",4,"AB","Groupe sanguin")
+AddEntry("hemato_parasitologie","gs","radio",4,"O","Groupe sanguin")
+AddEntry("hemato_parasitologie","rhesus","radio",2,"positif","Facteur rhesus ")
+AddEntry("hemato_parasitologie","rhesus","radio",2,"negatif","facteur rhesus")
+AddEntry("hemato_parasitologie","vs","text",1,"","")
+AddEntry("hemato_parasitologie","tp","text",1,"","")
+AddEntry("hemato_parasitologie","tca","text",1,"","")
+AddEntry("hemato_parasitologie","ts","text",1,"","")
+AddEntry("hemato_parasitologie","tx_reticul","text",1,"","")
+AddEntry("hemato_parasitologie","goutte_ep","radio",2,"positif","Goutte epaisse")
+AddEntry("hemato_parasitologie","goutte_ep","radio",2,"negatif","Goutte epaisse")
+AddEntry("hemato_parasitologie","autre","text",1,"","")
+AddEntry("hemato_parasitologie","technicien","text",1,"","")
+
+AddEntry("immuno_serologie","ag_hbs","text",1,"","")
+AddEntry("immuno_serologie","ac_hcv","text",1,"","")
+AddEntry("immuno_serologie","aslo","text",1,"","")
+AddEntry("immuno_serologie","crp","text",1,"","")
+AddEntry("immuno_serologie","hiv_det","text",1,"","")
+AddEntry("immuno_serologie","hiv_im","text",1,"","")
+AddEntry("immuno_serologie","chlamy","text",1,"","")
+AddEntry("immuno_serologie","tpha","text",1,"","")
+AddEntry("immuno_serologie","vdrl","text",1,"","")
+AddEntry("immuno_serologie","widal","text",1,"","")
+AddEntry("immuno_serologie","technicien","text",1,"","")
+
+
+
 
 C.commit()
 
-c.execute('.schema immuno_serologie')
+c.execute('select * from table_analyse;')
 
 for i in c:
     print "\n"
@@ -51,6 +99,23 @@ for i in c:
 
 
 
+
+#~ CreateTable():
+	#~ c.execute('''CREATE TABLE `bacteriologie` (
+  #~ `id` integer PRIMARY KEY,
+  #~ `ID_patient` int(15),
+  #~ `date` date,
+  #~ `type_examen` varchar(255),
+  #~ `macroscopie` text,
+  #~ `etat_frais` text,
+  #~ `comptage_cellules` int(15),
+  #~ `coloration_gram` text,
+  #~ `coloration_ziehl` text,
+  #~ `milieu_culture` text,
+  #~ `observation` text,
+  #~ `conclusion` text,
+  #~ `technicien` varchar(255));
+ #~ ''')
 
 
 #~ CREATE TABLE `biochimie` (
